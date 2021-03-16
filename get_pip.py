94dbc745b386c5d7c23986457a8e1fa18bebb062
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-03-16 14:00:37 +0600

    Worked on get_pip

--- get_pip.py
+++ get_pip.py
@@ -8,3 +8,15 @@ import tempfile
 # Useful for very coarse version differentiation.
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
+
+if PY3:
+    iterbytes = iter
+else:
+    def iterbytes(buf):
+        return (ord(byte) for byte in buf)
+
+try:
+    from base64 import b85decode
+except ImportError:
+    _b85alphabet = (b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    b"abcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~"
