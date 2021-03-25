32695fdc87b55d605dd026120d6b6b299ad5b61c
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-03-25 10:35:24 +0600

    Worked on Install Requirements

--- install_requirements.py
+++ install_requirements.py
@@ -4,3 +4,12 @@ import get_pip
 import os
 import importlib
 import contextlib
+
+
+def install(package):
+    '''
+    installs a package using pip
+
+    :param package: string
+    '''
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
