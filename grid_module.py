08f27af21c4f37c796bd7a07eef873bc89ff466a
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-03-28 11:11:57 +0600

    Grid Module Initialized

---
+++ grid_module.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+
+# Main abstract class (parent)
+# This class is capable of creating a grid containing different rows and different columns, bases upon those arguments it
+# will automatically alter the pixel size. To display the grid simply call ____.drawGrid(). To find the item in the grid
+# that was clicked on call ____.clicked().
