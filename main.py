65b5c589091b05a133111b78832775ae91a08d8b
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-04-17 04:37:40 +0600

    Worked on Main File

---
+++ main.py
@@ -0,0 +1,15 @@
+try:
+    import pygame
+except:
+    import install_requirements
+    import pygame
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import gridModule
+from gridModule import colorPallet
+from gridModule import pixelArt
+from gridModule import menu
+from gridModule import grid
+import sys
+import time
